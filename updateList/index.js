6ca346ec89e5878cfa83fd94eaef744de1f07b82
Code-Nat <44806486+Code-Nat@users.noreply.github.com>
2022-03-27 12:58:38 +0300

    Fixed excpected Parm to listTitle over title for incoming

--- updateList/index.js
+++ updateList/index.js
@@ -1,13 +1,35 @@
+const mongoDB = require("../shared/mongo");
+const Schemas = require("../shared/DBSchemas");
+const mongoose = require("mongoose");
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    const listID = (req.query.id || (req.body && req.body.id));
+    const listTitle = (req.query.listTitle || (req.body && req.body.listTitle));
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const connection = await mongoDB.connect();
+    const list = await connection.model('lists',Schemas.list);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    userID = new mongoose.Types.ObjectId('623c79e8626d52f11c600b5c');
+
+    const list_ID = new mongoose.Types.ObjectId(listID);
+
+    try {
+        result = await list.findOneAndUpdate({_id:list_ID,owningUser:userID},{
+            listTitle:listTitle
+        });
+
+        result = await list.find({_id:list_ID,owningUser:userID});
+
+        context.res = {
+            status:200,
+            body: result
+        };
+    }
+    catch (err)
+    {
+        context.res = {
+            status:400,
+            body: err
+        };
+    }
 }
