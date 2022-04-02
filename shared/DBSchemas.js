f65859d6e72d2dd687f06a2b168c88a208ca3427
Code-Nat <44806486+Code-Nat@users.noreply.github.com>
2022-04-02 21:49:45 +0300

    Exstanding title to allow for a single character

--- shared/DBSchemas.js
+++ shared/DBSchemas.js
@@ -48,11 +48,37 @@ const user = new mongoose.Schema({
     }
 });
 
+user.pre('save', async function () {
+  // console.log(this.modifiedPaths())
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+user.post('save', function(error, doc, next) {
+  if (error.name === 'MongoServerError' && error.code === 11000) {
+    next(new Error('There was a duplicate email error'));
+  } else {
+    next(error);
+  }
+});
+
+user.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME
+  });
+};
+
+user.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+}
+
 const list = new mongoose.Schema({
   listTitle: {
     type: String,
     required: [true, 'Please provide title'],
-    minlength: 3,
+    minlength: 1,
     maxlength: 20,
     trim: true
   },
@@ -81,30 +107,32 @@ const list = new mongoose.Schema({
   }]
 });
 
-user.pre('save', async function () {
-  // console.log(this.modifiedPaths())
-  if (!this.isModified('password')) return;
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
+list.methods.addTask = function (task) {
+  this.taskList.push(task);
+  return this;
+}
 
-user.post('save', function(error, doc, next) {
-  if (error.name === 'MongoServerError' && error.code === 11000) {
-    next(new Error('There was a duplicate email error'));
-  } else {
-    next(error);
+list.methods.removeTask = function (taskID) {
+  let index = this.taskList.findIndex((item => item.id == taskID));
+
+  if (~index)
+  {
+    this.taskList.splice (index, 1);
+    return this;
   }
-});
+  return undefined;
+}
 
-user.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME
-  });
-};
+list.methods.updateTask = function (updatedTask)
+{
+  let index = this.taskList.findIndex((item => item.id == updatedTask.id));
 
-user.methods.comparePassword = async function (candidatePassword) {
-  const isMatch = await bcrypt.compare(candidatePassword, this.password);
-  return isMatch;
+  if (~index)
+  {
+    this.taskList[index] = updatedTask;
+    return this;
+  }
+  return undefined;
 }
 
 module.exports = {
