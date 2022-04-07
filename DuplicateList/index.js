958c5f9fd8269f5758aa891e5e72fd95982365ab
Code-Nat <44806486+Code-Nat@users.noreply.github.com>
2022-04-07 14:37:48 +0300

    Added task duplicate

---
+++ DuplicateList/index.js
@@ -0,0 +1,51 @@
+const mongoDB = require("../shared/mongo");
+const Schemas = require("../shared/DBSchemas");
+const mongoose = require("mongoose");
+
+module.exports = async function (context, req) {
+    const listID = (req.query.listId || (req.body && req.body.listId));
+    //const listTitle = (req.query.listTitle || (req.body && req.body.listTitle));
+
+    if (!listID)
+    {
+        context.res = {
+            status:400,
+            body: "Missing list ID"
+        };
+        return;
+    }
+
+    userID = new mongoose.Types.ObjectId('623c79e8626d52f11c600b5c');
+
+    const list_ID = new mongoose.Types.ObjectId(listID);
+
+    const connection = await mongoDB.connect();
+    const list = await connection.model('lists',Schemas.list);
+
+    try {
+        let result = await list.findOne({_id:list_ID,owningUser:userID});
+
+        context.log (result);
+
+        result = await list.create({
+            listTitle: result.listTitle, 
+            dateCreated:new Date(), 
+            owningUser:userID,
+            taskList:result.taskList
+        });
+
+        context.res = {
+            status:200,
+            body: result
+        };
+    }
+    catch (err)
+    {
+        context.log (`error duplicating list: with listID=${listID} and listTitle=${listTitle} the error: ${err.message}`);
+        context.res = {
+            status:400,
+            body: err
+        };
+        return;
+    }
+}
