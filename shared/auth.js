73a0bf447a3f1bf812a2bcf324fe52d6c884aafb
Code-Nat <44806486+Code-Nat@users.noreply.github.com>
2022-04-08 11:29:39 +0300

    *Fixed Auth

--- shared/auth.js
+++ shared/auth.js
@@ -1,6 +1,6 @@
 const jwtUtil = require('./util/jwtUtil');
 
-const auth = async function auth (context) {
+module.exports = async function (context) {
     try {
         const token = context.req.headers.authorization.split(' ')[1];
         return await jwtUtil.verify(token);
@@ -9,13 +9,4 @@ const auth = async function auth (context) {
     {
         throw Error (err);
     }
-}
-
-const authUserID = function authUserID (context) {
-    return (await auth(context))._id;
-}
-
-module.exports = {
-    auth,
-    authUserID
 }
